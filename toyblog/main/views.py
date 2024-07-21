79e57bb409b5daa90cc12a17b0e496457f57ffcb
andy0000-droid <andy020207@gmail.com>
2024-07-21 16:36:16 +0900

    [DELETE] Django project directory

--- toyblog/main/views.py
+++
@@ -1,7 +0,0 @@
-from django.shortcuts import render
-from django.http import HttpResponse
-
-
-# Create your views here.
-def func(request):
-    return HttpResponse(request, "<h1> This is mainpage </h1>")
--- toyblog/post_write/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class PostWriteConfig(AppConfig):
-    default_auto_field = "django.db.models.BigAutoField"
-    name = "post_write"
