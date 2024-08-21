08b5427782c077c579b6cff389bb788bdc7edcfd
andy0000-droid <andy020207@gmail.com>
2024-08-21 19:47:14 +0900

    v0.0.8 Review code
    Reslove pylint message
    [EDIT]
    posting/urls
    posting/views
    writing/views

--- toyblog/posting/views.py
+++ toyblog/posting/views.py
@@ -4,13 +4,15 @@ from django.http import HttpResponse, JsonResponse
 
 # Create your views here.
 def show(request, postid):
+    """Function for show specific post"""
     return HttpResponse("<h1>" + str(postid) + " post page </h1>")
 
 
 def main(request):
+    """Function for show post mainpage"""
     return HttpResponse("<h1> Posting mainpage </h1>")
 
 
 def write(request):
-
+    """Function for show post writing page"""
     return HttpResponse("Post Writing page")
--- toyblog/writing/views.py
+++ toyblog/writing/views.py
@@ -4,4 +4,5 @@ from django.http import HttpResponse
 
 # Create your views here.
 def func(request):
+
     return HttpResponse("<h1> Writing page </h1>")
