99f289a443d4bb83c53ffb33d5ca4586de4c93e5
andy0000-droid <andy020207@gmail.com>
2024-08-21 15:48:05 +0900

    v0.0.7 - 2 Update databse
    [EDIT]
    main/models
    profile_conf/models

--- toyblog/main/models.py
+++ toyblog/main/models.py
@@ -4,4 +4,4 @@ from django.db import models
 # Create your models here.
 class Mainpage(models.Model):
     # component content #
-    pass
+    component = models.ImageField()
--- toyblog/profile_conf/models.py
+++ toyblog/profile_conf/models.py
@@ -4,11 +4,15 @@ from django.db import models
 # Create your models here.
 class Profile(models.Model):
     # addr # text
+    addr = models.TextField()
     # name # text
+    name = models.TextField()
     # about # text
+    about = models.TextField()
     # cat # text
+    cat = models.TextField()
     # img # bin
-    pass
+    img = models.ImageField()
     # Create
 
     # Read
